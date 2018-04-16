e7903c9bd6e7d2ab5e2f5f56919aa20b9cf8aa10
Unknown <bstanton773@gmail.com>
2018-04-16 15:47:16 -0500

    Add registration

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from app import app
+from app import app, db
 from flask import render_template, redirect, url_for, flash
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 from flask_login import current_user, login_user, logout_user
 
@@ -10,7 +10,7 @@ from flask_login import current_user, login_user, logout_user
 def index():
     return render_template('index.html', title='Home')
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET','POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -28,3 +28,19 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
