e7ef413517b1b579b9023419b18c1b1eb6f5335b
Unknown <bstanton773@gmail.com>
2018-04-16 14:17:51 -0500

    Connect to db

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,13 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from flask_login import LoginManager
+login = LoginManager(app)
+
 from app import routes
+from app import models
--- config.py
+++ config.py
@@ -7,5 +7,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
 
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:aaaaaa@localhost/movie'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:aaaaaa@localhost/movies'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
