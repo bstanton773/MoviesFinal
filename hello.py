aa155c0545a251556d62eae9f05d4e7dc20d3d08
Unknown <bstanton773@gmail.com>
2018-04-16 11:02:24 -0500

    Refactoring

--- hello.py
+++ hello.py
@@ -1,10 +1,3 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
+from app import app
 
 app.run()
