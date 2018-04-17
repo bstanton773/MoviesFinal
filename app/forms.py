6715b2aa93a99ee3b29088ca08253f489d86af6b
Unknown <bstanton773@gmail.com>
2018-04-17 16:44:05 -0500

    Search db for movies

--- app/forms.py
+++ app/forms.py
@@ -28,3 +28,7 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
+
+class SearchForm(FlaskForm):
+    search = StringField('Movies', validators=[DataRequired()])
+    submit = SubmitField('Search')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app, db
 from flask import render_template, redirect, url_for, flash
 from app.models import Movies
+from app.forms import SearchForm
 
 
 
@@ -9,3 +10,12 @@ from app.models import Movies
 def index():
     movies = Movies.query.all()
     return render_template('index.html', title='Home', movies = movies)
+
+@app.route('/search', methods=['GET','POST'])
+def search():
+    form = SearchForm()
+    if form.search.data is not None:
+        movies = Movies.query.filter(Movies.title.like('%' + form.search.data + '%')).all()
+    else:
+        movies = Movies.query.all()
+    return render_template('search.html', form=form, movies=movies)
