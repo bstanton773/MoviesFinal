6f3d4940e33e9793e4166fbbafbb486aa364241f
Unknown <bstanton773@gmail.com>
2018-04-18 10:54:54 -0500

    Add/Remove watchlist

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app, db
 from flask import render_template, redirect, url_for, flash
-from app.models import Movies
+from app.models import Movies, Reviews
 from app.forms import SearchForm
 from flask_login import current_user
 
@@ -27,3 +27,33 @@ def review(movieId):
         return redirect(url_for('login'))
     movie = Movies.query.filter_by(movieId = movieId).first()
     return render_template('review.html', movie = movie, title='Review')
+
+@app.route('/watchlist')
+def watchlist():
+    if current_user.is_anonymous:
+        return redirect(url_for('login'))
+    movies_in_watchlist = Reviews.query.filter_by(user_id = current_user.id, watchlist=1).join(
+        Movies, Reviews.movie_id == Movies.movieId).add_columns(
+        Movies.title, Movies.genres, Movies.year, Movies.movieId).all()
+    return render_template('watchlist.html', title='My Watchlist', movies = movies_in_watchlist)
+
+@app.route('/addToWatchlist/<int:movieId>/<string:from_page>')
+def addToWatchlist(movieId, from_page):
+    if current_user.is_anonymous:
+        return redirect(url_for('login'))
+    watch = Reviews(user_id = current_user.id, movie_id = movieId, watchlist=1)
+    db.session.add(watch)
+    db.session.commit()
+    return redirect(url_for('search'))
+
+@app.route('/removeFromWatchlist/<int:movieId>/<string:from_page>')
+def removeFromWatchlist(movieId, from_page):
+    if current_user.is_anonymous:
+        return redirect(url_for('login'))
+    watch = Reviews.query.filter_by(user_id = current_user.id, movie_id = movieId, watchlist=1).first()
+    db.session.delete(watch)
+    db.session.commit()
+    if from_page == 'watchlist':
+        return redirect(url_for('watchlist'))
+    else:
+        return redirect(url_for('search'))
