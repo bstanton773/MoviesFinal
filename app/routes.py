7652432df4cf8355ab273835e4e6a7339a9a6dba
Unknown <bstanton773@gmail.com>
2018-04-18 09:52:15 -0500

    Added movie review page

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from app import app, db
 from flask import render_template, redirect, url_for, flash
 from app.models import Movies
 from app.forms import SearchForm
+from flask_login import current_user
 
 
 
@@ -18,4 +19,11 @@ def search():
         movies = Movies.query.filter(Movies.title.like('%' + form.search.data + '%')).all()
     else:
         movies = Movies.query.limit(10)
-    return render_template('search.html', form=form, movies=movies)
+    return render_template('search.html', form=form, movies=movies, title='Search')
+
+@app.route('/review/<int:movieId>')
+def review(movieId):
+    if current_user.is_anonymous:
+        return redirect(url_for('login'))
+    movie = Movies.query.filter_by(movieId = movieId).first()
+    return render_template('review.html', movie = movie, title='Review')
